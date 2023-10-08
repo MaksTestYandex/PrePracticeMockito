46b64e2c728ab606ba5297784eb19dfa84365863
mablinov <mablinov@at-consulting.ru>
2023-10-08 22:02:31 +0300

    Add MockPrictice

--- src/test/java/com/example/MockStabTest.java
+++ src/test/java/com/example/MockStabTest.java
@@ -1,4 +1,5 @@
 package com.example;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,16 +17,16 @@ public class MockStabTest {
 
     @Test
     public void test() {
-        Mockito.when(cat.createHalfCat()).thenReturn(new Cat(2,"Я самый умный"));
-        Mockito.when(dog.createHalfDog()).thenReturn(new Dog(2,"Я самый весёлый"));
+        Mockito.when(cat.createHalfCat()).thenReturn(new Cat(2, "Я самый умный"));
+        Mockito.when(dog.createHalfDog()).thenReturn(new Dog(2, "Я самый весёлый"));
 // стаб, возвращающий половину кота с параметрами - 2 лапы, говорит "Я самый умный"
 //        ...// стаб, возвращающий половину пса с параметрами - 2 лапы, говорит "Я самый весёлый"
         Cat halfCat = cat.createHalfCat();
         Dog halfDog = dog.createHalfDog();
         CatDog catDog = new CatDog(halfCat, halfDog);
-        Assert.assertEquals(catDog.countLegs(2,2),4);
-        Assert.assertEquals(catDog.getSound(),"Единственный в мире малыш Котопёс");
-    //    ...// проверка, что у Котопса 4 лапы
-  //      ...// проверка, что Котопёс говорит "Единственный в мире малыш Котопёс"
+        Assert.assertEquals(catDog.countLegs(2, 2), 4);
+        Assert.assertEquals(catDog.getSound(), "Единственный в мире малыш Котопёс");
+        //    ...// проверка, что у Котопса 4 лапы
+        //      ...// проверка, что Котопёс говорит "Единственный в мире малыш Котопёс"
     }
 }
